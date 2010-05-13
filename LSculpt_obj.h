e75ff94e43b8cd6e49f12fd9c40c506f06def9fb
remigagne <remigagne@bd47c8df-1908-ce94-e918-49178dadd77f>
2010-05-13 19:48:58 +0000

    Add general GPL comment header to each source file.

--- LSculpt_obj.h
+++ LSculpt_obj.h
@@ -1,3 +1,24 @@
+/*	LSculpt: Studs-out LEGO® Sculpture
+
+	Copyright (C) 2010 Bram Lambrecht <bram@bldesign.org>
+
+	http://lego.bldesign.org/LSculpt/
+
+	This file (LSculpt_obj.h) is part of LSculpt.
+
+	LSculpt is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	LSculpt is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see http://www.gnu.org/licenses/  */
+
 #ifndef OBJ_HPP_INCLUDED
 #define OBJ_HPP_INCLUDED
 
